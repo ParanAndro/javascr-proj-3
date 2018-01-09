142ec6ef07e6213f907581b9fe3f34fe9e5797f6
Radek Izdebski <radek1izdebski@gmail.com>
2018-01-09 20:28:13 +0100

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var a = 6,
+    b = 4,
+   	value = (a * a) - (2 * a * b) + (b * b);
+   	console.log(('Equation: ('+ a + '*' + a + ') - (2*' + a + '*' + b + ') + (' + b + '*' + b + ') gives result: ' + value));
+if  ( value < 0 ) {
+	console.log('Result is negative');
+}	else if ( value > 0) {
+	console.log('Result is positive');
+}
+if ( value == 0) {
+	console.log('Result = 0');
+}	else if (( value < 0 ) || ( value > 0 )) {
+	console.log('Result is not 0');
+}
